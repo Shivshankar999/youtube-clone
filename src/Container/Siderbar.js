4492c571dfca00bfa4160029388578a1051835e2
shiv fol <shivshankarkumarchaudhary587@gmail.com>
2024-10-29 05:01:14 +0530

    3rd

--- src/Container/Siderbar.js
+++ src/Container/Siderbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MenuItem from "./MenuItem";
+
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
--- src/utils/containts.js
+++
@@ -1,8 +0,0 @@
-
-
-export const YOUTUBE_VIDEO_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  GOOGLE_API_KEY;
-
-export const YOUTUBE_SUGGUSSTION_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
